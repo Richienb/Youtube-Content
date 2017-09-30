f0e2704f85bb22890ac0917731a0ac30bb6cd115
Yash Kumar Verma <yk.verma2000@gmail.com>
2017-09-30 11:49:39 +0530

    test

---
+++ Single Command Git add, commit and Push/code.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	char command[200];
+	strcpy(command,"git add . & git commit -m ");
+
+	if(argc == 2){
+		strcat(command,argv[1]);	
+		strcat(command, " & git push origin master");
+	    system(command);
+	    cout << "\n Commited and Pushed ! \n";
+	}
+	else{
+		cout << "\n\nEnter text to commit. \n\n";
+		cout << "Something like this would work like a charm : <name of executable> \"<text for commit>\" \n\n" << endl;
+	}
+
+    return 1;
+}
